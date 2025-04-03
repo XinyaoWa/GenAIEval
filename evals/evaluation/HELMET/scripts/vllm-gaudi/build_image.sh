75a7001699aca5ee25f8e6a490033b2b3093f65f
Xinyao Wang <xinyao.wang@intel.com>
2025-04-03 13:40:47 +0800

    Update HELMET to support vllm-gaudi
    Support OpenAI API compatiable endpoint such as vllm & tgi
    Validate on Llama3.3-70b-Instruct
    
    Signed-off-by: Xinyao Wang <xinyao.wang@intel.com>

--- evals/evaluation/HELMET/scripts/vllm-gaudi/build_image.sh
+++ evals/evaluation/HELMET/scripts/vllm-gaudi/build_image.sh
@@ -1,10 +1,11 @@
 # Copyright (C) 2024 Intel Corporation
 # SPDX-License-Identifier: Apache-2.0
 
+export TAG="helmet"
 echo "Building the vllm-gaudi docker images"
 git clone https://github.com/HabanaAI/vllm-fork.git
 cd ./vllm-fork
-git checkout v0.6.4.post2+Gaudi-1.19.0 #habana_main
+git checkout v0.6.4.post2+Gaudi-1.19.1 #habana_main
 
 docker build --no-cache -f Dockerfile.hpu -t ${REGISTRY:-opea}/vllm-gaudi:${TAG:-latest} --shm-size=128g . --build-arg https_proxy=$https_proxy --build-arg http_proxy=$http_proxy
 if [ $? -ne 0 ]; then
